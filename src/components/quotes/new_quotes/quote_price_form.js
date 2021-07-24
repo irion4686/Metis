bb966e0934d543f6de8bd096af942efe4ef3b6bb
Cody Irion <irion@live.unc.edu>
2021-07-24 17:21:12 -0400

    More CSS adjustments

--- src/components/quotes/new_quotes/quote_price_form.js
+++ src/components/quotes/new_quotes/quote_price_form.js
@@ -1,18 +1,31 @@
+import classes from './quote_price_form.module.css';
 
 const QuotePrice = (props) => {
     return (
-        <div>
+        <div className={classes.price}>
+
             <h2>Pricing</h2>
-            <label>Base Price</label>
-            <input type='number'/>
-            <label>Discount</label>
-            <input type='number'/>
-            <label>Overnight Costs</label>
-            <input type='number'/>
-            <label>Misc Costs</label>
-            <input type='number'/>
-            <label>Total Price</label>
-            <input type='number'/>
+            <div>
+                <label>Base Price</label>
+                <input type='number'/>
+            </div>
+            <div>
+                <label>Discount</label>
+                <input type='number'/>
+            </div>
+            <div>
+                <label>Overnight Costs</label>
+                <input type='number'/>
+            </div>
+            <div>
+                <label>Misc Costs</label>
+                <input type='number'/>
+            </div>
+            <div>
+                <label>Total Price</label>
+                <input type='number'/>
+            </div>
+
         </div>
     )
 }
